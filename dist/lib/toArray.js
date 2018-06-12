29aff27dd96eb4e126c0c17760f86a686579436f
Javier Perales <javier@agroprediccion.es>
2018-06-12 12:40:43 +0200

    version 1.0.1

---
+++ dist/lib/toArray.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function toArray(element) {
+    return Array.isArray(element) ? element : [element];
+}
+exports.toArray = toArray;
